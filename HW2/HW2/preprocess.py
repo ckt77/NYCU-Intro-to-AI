344811a72509116b2bd8f6bfc9277dc88cbea1e8
陳士弘 <shihhong25@gmail.com>
2023-06-18 00:00:11 +0800

    HW2

---
+++ HW2/HW2/preprocess.py
@@ -0,0 +1,32 @@
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.tokenize.toktok import ToktokTokenizer
+
+
+def remove_stopwords(text: str) -> str:
+    '''
+    E.g.,
+        text: 'Here is a dog.'
+        preprocessed_text: 'Here dog.'
+    '''
+    stop_word_list = stopwords.words('english')
+    tokenizer = ToktokTokenizer()
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    filtered_tokens = [token for token in tokens if token.lower() not in stop_word_list]
+    preprocessed_text = ' '.join(filtered_tokens)
+
+    return preprocessed_text
+
+
+def preprocessing_function(text: str) -> str:
+    preprocessed_text = remove_stopwords(text)
+    
+    # TO-DO 0: Other preprocessing function attemption
+    # Begin your code 
+
+    # End your code
+
+    return preprocessed_text
